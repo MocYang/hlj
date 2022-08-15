ee4dae8fec4427aef0bbe18d0c8098b86e72ea15
MocYang <958292256@qq.com>
2022-08-15 09:51:50 +0800

    feat:增加监控列表假数据文件

---
+++ src/api/camera.js
@@ -0,0 +1,251 @@
+export default {
+  "code": 0,
+  "msg": "success",
+  "data": [{
+    "id": "A0F5302B1A6BF94ACC980B4F7E10330E",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "",
+    "type_name": "半球",
+    "device_code": "22222",
+    "device_name": "1楼222",
+    "model_url": "TEMP_SHAPE_D4",
+    "position": {},
+    "center": {
+      "x": -45466.359375,
+      "y": -18532.2734375,
+      "z": 1148.623046875,
+      "pitch": 51.53987503051758,
+      "yaw": -179.2614288330078,
+      "roll": 0.0005265994113869965,
+      "distance": 100
+    },
+    "list_style": {
+      "x": -46086.97265625,
+      "y": -18454.279296875,
+      "z": 460.70989990234375,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F001",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-45466.359375, -18532.2734375]
+    },
+    "order": 126,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }, {
+    "id": "639928F3765D5742F24A1876BE6AC732",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "",
+    "type_name": "半球",
+    "device_code": "3222222",
+    "device_name": "3楼2222",
+    "model_url": "TEMP_SHAPE_D28",
+    "position": {},
+    "center": {
+      "x": -45026.875,
+      "y": -19262.755859375,
+      "z": 1814.052978515625,
+      "pitch": 28.320510864257812,
+      "yaw": -176.63575744628906,
+      "roll": 0.0003786690649576485,
+      "distance": 100
+    },
+    "list_style": {
+      "x": -46282.0390625,
+      "y": -19687.373046875,
+      "z": 1063.1214599609375,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F003",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-45026.875, -19262.755859375]
+    },
+    "order": 130,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }, {
+    "id": "56C8B20ED9F6CC4BD0C8D0FCA0789AFB",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "10001_02_03",
+    "type_name": "半球",
+    "device_code": "1111",
+    "device_name": "1楼11",
+    "model_url": "TEMP_SHAPE_D2",
+    "position": {},
+    "center": {
+      "x": -45597.984375,
+      "y": -18198.5234375,
+      "z": 2058.295654296875,
+      "pitch": 75.68868255615234,
+      "yaw": 176.80044555664062,
+      "roll": 0.001322418567724526,
+      "distance": 100
+    },
+    "list_style": {
+      "x": "-45958.96875",
+      "y": "-17902.119140625",
+      "z": 458.72760009765625,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F001",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-45597.984375, -18198.5234375]
+    },
+    "order": 125,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }, {
+    "id": "CBC9ED4D3956164BEC883484D5567748",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "",
+    "type_name": "半球",
+    "device_code": "311111",
+    "device_name": "3楼111",
+    "model_url": "TEMP_SHAPE_D26",
+    "position": {},
+    "center": {
+      "x": -45026.875,
+      "y": -19262.755859375,
+      "z": 1814.052978515625,
+      "pitch": 28.320510864257812,
+      "yaw": -176.63575744628906,
+      "roll": 0.0003786690649576485,
+      "distance": 100
+    },
+    "list_style": {
+      "x": -46282.0390625,
+      "y": -18997.61328125,
+      "z": 1055.1263427734375,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F003",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-45026.875, -19262.755859375]
+    },
+    "order": 129,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }, {
+    "id": "E6A86F2AAD22604E129A3B370D9487D2",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "10001_02_03",
+    "type_name": "半球",
+    "device_code": "222",
+    "device_name": "2楼",
+    "model_url": "TEMP_SHAPE_D10",
+    "position": {},
+    "center": {
+      "x": -45977.15234375,
+      "y": -17780.73046875,
+      "z": 1821.39990234375,
+      "pitch": 28.320510864257812,
+      "yaw": -176.63575744628906,
+      "roll": 0.0003786690649576485,
+      "distance": 100
+    },
+    "list_style": {
+      "x": -47541.953125,
+      "y": -17734.548828125,
+      "z": 770.0421142578125,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F002",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-45977.15234375, -17780.73046875]
+    },
+    "order": 127,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }, {
+    "id": "179C4C7AE10FA0474DC9895EE6A877DD",
+    "category_id": "10001",
+    "category_name": "摄像机",
+    "type_id": "10001_02_03",
+    "type_name": "半球",
+    "device_code": "222223333",
+    "device_name": "2楼222",
+    "model_url": "TEMP_SHAPE_D22",
+    "position": {},
+    "center": {
+      "x": -46513.61328125,
+      "y": -17958.98828125,
+      "z": 1116.195556640625,
+      "pitch": 28.320510864257812,
+      "yaw": -176.63575744628906,
+      "roll": 0.0003786690649576485,
+      "distance": 100
+    },
+    "list_style": {
+      "x": -47539.43359375,
+      "y": -17731.5390625,
+      "z": 770.0421142578125,
+      "pitch": "",
+      "yaw": 0,
+      "roll": ""
+    },
+    "enable": false,
+    "region_id": "D6045B814A4EEC4B9EA807C756FC81AD",
+    "build_id": "V001_JZ0001",
+    "floor_id": "V001_JZ0001#F002",
+    "indoor": true,
+    "model_name": "banqiu",
+    "geom": {
+      "type": "Point",
+      "coordinates": [-46513.61328125, -17958.98828125]
+    },
+    "order": 128,
+    "grid_id": null,
+    "grid_info": {},
+    "detail_info": null
+  }]
+}
